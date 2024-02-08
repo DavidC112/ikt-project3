d3d606f07f35095759c7a348180ce59607a636a2
DavidC112 <cornutadavid0@gmail.com>
2024-02-08 12:29:28 +0100

    karakterkésztő haladás

---
+++ Assets/_Scripts/RaceStats.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "RaceStats")]
+public class RaceStats : ScriptableObject
+{
+    //To Do: spritokat
+    public Sprite raceSprite;
+
+    public int vigPoint;
+    public int strPoint;
+    public int dexPoint;
+    public int endPoint;
+    public int intPoint;
+}
