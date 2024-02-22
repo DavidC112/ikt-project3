0ff7e314ffee714f1c3c0afd546e738b6d287f54
DavidC112 <cornutadavid0@gmail.com>
2024-02-22 08:21:47 +0100

    TownHall_
    movement, camera

---
+++ Assets/_Scripts/movement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 10.5f;
+    private Rigidbody2D rigidb;
+
+    void Start()
+    {
+        rigidb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rigidb.velocity = movement * speed;
+    }
+}
+
